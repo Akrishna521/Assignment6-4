168f11bca4eb59eeee0c269a60e0f2a5921885f2
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-11-10 16:05:02 +0530

    Assignment6-4

---
+++ ConAppAssignmentGradeRemoval/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Sample student grades data
+        List<int> studentGrades = new List<int> { 85, 90, 78, 92, 88 };
+
+        // Task 4.1: Remove the grade of 78 from studentGrades
+        studentGrades.Remove(78);
+
+        // Task 4.2: Display the studentGrades after the removal
+        Console.WriteLine("Student Grades after removing 78:");
+        foreach (int grade in studentGrades)
+        {
+            Console.WriteLine(grade);
+        }
+    }
+}
